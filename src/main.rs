cb6c799a1652c8cab8327847a4e53869f22a8fad
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-20 20:38:59 +0200

    Print out input files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let mut args = env::args();
+    // Discard program name
+    args.next();
+    let path = args.next().expect("Please give me a file path to run");
+    let src = fs::read_to_string(path)?;
+    print!("{}", src);
+    Ok(())
 }
