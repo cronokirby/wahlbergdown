040e59bc03e94888c3f6ad4ffa55f30a573099da
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-20 21:36:27 +0200

    Setup structopt

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,44 @@ mod lexer;
 
 use std::env;
 use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
 
-fn main() -> std::io::Result<()> {
-    let mut args = env::args();
-    // Discard program name
-    args.next();
-    let path = args.next().expect("Please give me a file path to run");
-    let src = fs::read_to_string(path)?;
-    print!("{}", src);
-    Ok(())
+use structopt::StructOpt;
+
+/// A command that our CLI can process
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Print the tokens produced by the lexer.
+    Lex {
+        /// The file containing Wahlbergdown code you want to lex.
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    /// Print the AST produced by the parser.
+    Parse {
+        /// The file containing Wahlbergdown code you want to parse.
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    // Run a file.
+    Run {
+        /// The file containing Wahlbergdown code you want to run.
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+}
+
+fn run(input_file: &Path) {
+    let src = fs::read_to_string(input_file).expect("failed to read input file");
+    print!("{}", src)
+}
+
+fn main() {
+    let args = Command::from_args();
+    match args {
+        Command::Lex { .. } => eprintln!("unimplemented"),
+        Command::Parse { .. } => eprintln!("unimplemented"),
+        Command::Run { input_file } => run(&input_file),
+    }
 }
