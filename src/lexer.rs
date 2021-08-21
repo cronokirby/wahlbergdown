f396f3a15abac47e2dedd117726416e191176318
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-21 10:08:35 +0200

    Define Lexer type

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,14 +1,30 @@
-// Represents a single token produced by our lexer
+/// Represents a single token produced by our lexer
 #[derive(Clone, Debug, PartialEq)]
 enum Token {
-    // <!--
+    /// <!--
     CommentOpen,
-    // -->
+    /// -->
     CommentClose,
-    // `
+    /// `
     Tick,
-    // The \n character
+    /// The \n character
     Newline,
-    // Everything else is just raw text.
+    /// Everything else is just raw text.
     Raw(String),
+}
+
+/// A Lexer uses our source code to emit tokens.
+#[derive(Debug)]
+struct Lexer {
+    /// The source code for our program.
+    src: String,
+    /// The current position in our source code.
+    pos: usize,
+}
+
+impl Lexer {
+    /// Create a new lexer with some source code.
+    fn new(src: String) -> Self {
+        Lexer { src, pos: 0 }
+    }
 }
